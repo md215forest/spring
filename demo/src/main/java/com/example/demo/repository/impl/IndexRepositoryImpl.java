44847d7d931cf32ad027188b6583434a1fe8b7df
mori daiki <mori@zyyx.jp>
2023-08-12 07:57:45 +0900

    initial build

---
+++ demo/src/main/java/com/example/demo/repository/impl/IndexRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository.impl;
+
+import com.example.demo.repository.IndexRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class IndexRepositoryImpl implements IndexRepository {
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/IndexServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.IndexService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IndexServiceImpl implements IndexService {
+}
